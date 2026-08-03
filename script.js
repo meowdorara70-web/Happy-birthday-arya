dc90f193136c1fad8331449087f42264bae47acb
meowdorara70-web <meowdorara70@gmail.com>
2026-08-04 00:32:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -144,12 +144,12 @@ const photoImg = document.getElementById("photoImg");
 const photoPlaceholder = document.getElementById("photoPlaceholder");
 const testImg = new Image();
 testImg.onload = ()=>{
-  photoImg.src = "assets/photo.jpg";
+  photoImg.src = "Assets/photo.jpg";
   photoImg.classList.add("has-src");
   photoPlaceholder.style.display = "none";
 };
 testImg.onerror = ()=>{ /* keep placeholder */ };
-testImg.src = "assets/photo.jpg";
+testImg.src = "Assets/photo.jpg";
 
 /* =========================================================
    CAKE SCENE
